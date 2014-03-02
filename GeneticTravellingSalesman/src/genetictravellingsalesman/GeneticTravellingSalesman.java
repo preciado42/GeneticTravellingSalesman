65b77e06be33cfce4d0045d60524defceeb78646
Room <Room@Room-PC>
2014-03-01 19:14:24 -0700

    initializing the program files

---
+++ GeneticTravellingSalesman/src/genetictravellingsalesman/GeneticTravellingSalesman.java
@@ -0,0 +1,23 @@
+/**
+ * This program will implement a genetic algorithm in the hopes of solving the
+ * knapsack problem through trial and error. Values will be hardcoded for now
+ * but in the future may be dynamic.
+ *
+ * Bio Computing Dr. Gurka February 7 2014
+ *
+ * @author Angel Preciado
+ */
+
+package genetictravellingsalesman;
+
+
+public class GeneticTravellingSalesman {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
